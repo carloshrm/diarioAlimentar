0f4f540daf46aa99dcc6b9aded8685463eecf19f
Carlos Moraes <carloshrmoraes@gmail.com>
2023-05-12 16:55:01 -0300

    atualizando modelo de usuario com informações completas

--- Server/Models/ApplicationUser.cs
+++ Server/Models/ApplicationUser.cs
@@ -1,8 +1,21 @@
 ﻿using Microsoft.AspNetCore.Identity;
 
-namespace diarioAlimentar.Server.Models
+namespace diarioAlimentar.Server.Models;
+
+public class ApplicationUser : IdentityUser
 {
-    public class ApplicationUser : IdentityUser
-    {
-    }
+    [PersonalData]
+    public DateTime dataNascimento { get; set; }
+
+    [PersonalData]
+    public double peso { get; set; }
+
+    [PersonalData]
+    public double altura { get; set; }
+
+    [PersonalData]
+    public NivelAtividade nivelAtividade { get; set; }
+
+    [PersonalData]
+    public Sexo sexo { get; set; }
 }
