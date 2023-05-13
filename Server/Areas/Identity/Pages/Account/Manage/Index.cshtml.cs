b25c6806d6ee487cb4d1a58e3a94b929e726e570
Carlos Moraes <carloshrmoraes@gmail.com>
2023-05-13 09:56:44 -0300

    adicionando scaffolding da auth para extensão do cadastro do usuário

---
+++ Server/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -0,0 +1,150 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+#nullable disable
+
+using System.ComponentModel.DataAnnotations;
+
+using diarioAlimentar.Server.Models;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace diarioAlimentar.Server.Areas.Identity.Pages.Account.Manage
+{
+    public class IndexModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public IndexModel(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public class InputModel
+        {
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+
+            [Required]
+            [Display(Name = "Data de Nascimento")]
+            [DataType(DataType.Date)]
+            public DateTime dataNascimento { get; set; }
+
+            [Required]
+            [Display(Name = "Peso")]
+            public double peso { get; set; }
+
+            [Required]
+            [DataType(DataType.Text)]
+            [Display(Name = "Altura")]
+            public double altura { get; set; }
+
+            [Required]
+            [DataType("NivelAtividade")]
+            [Display(Name = "Nível de Atividade")]
+            public NivelAtividade nivelAtividade { get; set; }
+
+            [Required]
+            [DataType("Sexo")]
+            [Display(Name = "Sexo")]
+            public Sexo sexo { get; set; }
+
+        }
+
+        private async Task LoadAsync(ApplicationUser user)
+        {
+            var userName = await _userManager.GetUserNameAsync(user);
+            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+
+            Username = userName;
+
+            Input = new InputModel
+            {
+                dataNascimento = user.dataNascimento,
+                peso = user.peso,
+                altura = user.altura,
+                nivelAtividade = user.nivelAtividade,
+                sexo = user.sexo,
+            };
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            await LoadAsync(user);
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                await LoadAsync(user);
+                return Page();
+            }
+
+            if (Input.dataNascimento != user.dataNascimento)
+                user.dataNascimento = Input.dataNascimento;
+
+            if (Input.peso != user.peso)
+                user.peso = Input.peso;
+
+            if (Input.altura != user.altura)
+                user.altura = Input.altura;
+
+            if (Input.nivelAtividade != user.nivelAtividade)
+                user.nivelAtividade = Input.nivelAtividade;
+
+            if (Input.sexo != user.sexo)
+                user.sexo = Input.sexo;
+
+            await _userManager.UpdateAsync(user);
+
+            await _signInManager.RefreshSignInAsync(user);
+            StatusMessage = "Your profile has been updated";
+            return RedirectToPage();
+        }
+    }
+}
