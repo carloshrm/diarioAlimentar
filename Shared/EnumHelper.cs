e8cd2376cade48f56415d12869e00e50754b5426
Carlos Moraes <carloshrmoraes@gmail.com>
2023-05-26 11:28:41 -0300

    extensão para pegar string definida para enum

---
+++ Shared/EnumHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diarioAlimentar.Shared
+{
+    public static class EnumHelper
+    {
+        public static string GetEnumDisplay(this Enum e)
+        {
+            return .GetType().GetMember(e.ToString())
+                   .First()
+                   .GetCustomAttribute<DisplayAttribute>()
+                   .Name;
+        }
+    }
+}
