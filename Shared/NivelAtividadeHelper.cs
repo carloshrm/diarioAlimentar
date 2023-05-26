03a2eaf663e94e62c5919ed1c3462d27aef02a24
Carlos Moraes <carloshrmoraes@gmail.com>
2023-05-26 11:31:01 -0300

    atualizando estilização

---
+++ Shared/NivelAtividadeHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace diarioAlimentar.Shared;
+
+public static class NivelAtividadeHelper
+{
+    public static double GetValorAtividade(NivelAtividade n)
+    {
+        return n switch
+        {
+            NivelAtividade.Sedentario => 1.2,
+            NivelAtividade.AtivoLeve => 1.375,
+            NivelAtividade.AtivoModerado => 1.55,
+            NivelAtividade.AtivoMuito => 1.725,
+            NivelAtividade.AtivoExtremo => 1.9,
+        };
+    }
+}
--- Shared/Periodo.cs
+++ Shared/Periodo.cs
@@ -1,9 +1,18 @@
-﻿namespace diarioAlimentar.Shared;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace diarioAlimentar.Shared;
 
 public enum Periodo
 {
+    [Display(Name = "Café da Manhã")]
     Manha = 0,
+
+    [Display(Name = "Almoço")]
     Almoco = 1,
+
+    [Display(Name = "Jantar")]
     Janta = 2,
+
+    [Display(Name = "Lanche")]
     Lanche = 3
 };
