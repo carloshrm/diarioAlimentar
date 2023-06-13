7fb0925da1b5715ada729c8b890b092b08ebc86d
Carlos Moraes <carloshrmoraes@gmail.com>
2023-06-13 15:49:22 -0300

    validação

--- Server/Controllers/RefeicaoController.cs
+++ Server/Controllers/RefeicaoController.cs
@@ -3,59 +3,58 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace diarioAlimentar.Server.Controllers
+namespace diarioAlimentar.Server.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("[controller]")]
+public class RefeicaoController : ControllerBase
 {
-    [Authorize]
-    [ApiController]
-    [Route("[controller]")]
-    public class RefeicaoController : ControllerBase
+    private readonly ApplicationDbContext _ctx;
+
+    public RefeicaoController(ApplicationDbContext diarioContext)
     {
-        private readonly ApplicationDbContext _ctx;
+        _ctx = diarioContext;
+    }
 
-        public RefeicaoController(ApplicationDbContext diarioContext)
+    [HttpPost("add")]
+    public async Task<ActionResult<Diario>> AddRefeicao(Refeicao rf)
+    {
+        var refExistente = _ctx.Refeicoes.FirstOrDefault(r => r.refeicaoID == rf.refeicaoID);
+        if (refExistente != null)
+            _ctx.Refeicoes.Update(rf);
+        else
         {
-            _ctx = diarioContext;
+            _ctx.Refeicoes.Add(rf);
+            foreach (var p in rf.porcoes)
+                _ctx.Porcoes.Add(p);
+        }
+        await _ctx.SaveChangesAsync();
+        return Ok(rf);
+    }
+
+    [HttpPost("upd")]
+    public async Task<ActionResult<Diario>> UpdateRefeicao(Refeicao rf)
+    {
+        var refExistente = _ctx.Refeicoes.FirstOrDefault(r => r.refeicaoID == rf.refeicaoID);
+        if (refExistente == null)
+            return BadRequest();
+        else
+        {
+            _ctx.Refeicoes.Update(rf);
+            _ctx.Porcoes.Where(p => p.refeicaoID == rf.refeicaoID).ToList();
         }
 
-        [HttpPost("add")]
-        public async Task<ActionResult<Diario>> AddRefeicao(Refeicao rf)
-        {
-            var refExistente = _ctx.Refeicoes.FirstOrDefault(r => r.refeicaoID == rf.refeicaoID);
-            if (refExistente != null)
-                _ctx.Refeicoes.Update(rf);
-            else
-            {
-                _ctx.Refeicoes.Add(rf);
-                foreach (var p in rf.porcoes)
-                    _ctx.Porcoes.Add(p);
-            }
-            await _ctx.SaveChangesAsync();
-            return Ok(rf);
-        }
+        await _ctx.SaveChangesAsync();
+        return Ok(refExistente);
+    }
 
-        [HttpPost("upd")]
-        public async Task<ActionResult<Diario>> UpdateRefeicao(Refeicao rf)
-        {
-            var refExistente = _ctx.Refeicoes.FirstOrDefault(r => r.refeicaoID == rf.refeicaoID);
-            if (refExistente == null)
-                return BadRequest();
-            else
-            {
-                _ctx.Refeicoes.Update(rf);
-                _ctx.Porcoes.Where(p => p.refeicaoID == rf.refeicaoID).ToList();
-            }
-
-            await _ctx.SaveChangesAsync();
-            return Ok(refExistente);
-        }
-
-        [HttpGet("del/{rID}")]
-        public async Task<ActionResult<Diario>> RemoveRefeicao(Guid rID)
-        {
-            var refDB = _ctx.Refeicoes.First(r => r.refeicaoID == rID);
-            _ctx.Refeicoes.Remove(refDB);
-            await _ctx.SaveChangesAsync();
-            return Ok();
-        }
+    [HttpGet("del/{rID}")]
+    public async Task<ActionResult<Diario>> RemoveRefeicao(Guid rID)
+    {
+        var refDB = _ctx.Refeicoes.First(r => r.refeicaoID == rID);
+        _ctx.Refeicoes.Remove(refDB);
+        await _ctx.SaveChangesAsync();
+        return Ok();
     }
 }
